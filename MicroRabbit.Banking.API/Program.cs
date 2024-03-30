4aa476750811acd1c02afc4767064dd9414ad6fd
M1thr4ndir <sagaalejo@gmail.com>
2024-03-30 02:20:25 -0300

    feat: Configurar contexto de base de datos en la aplicación
    Este commit agrega la configuración del contexto de la base de datos utilizando Entity Framework Core para la aplicación. Se establece la conexión con la base de datos utilizando la cadena de conexión "BankingDBConnection" especificada en el archivo de configuración. Además, se registran los servicios necesarios en el contenedor de dependencias a través del contenedor DependencyContainer.

--- MicroRabbit.Banking.API/Program.cs
+++ MicroRabbit.Banking.API/Program.cs
@@ -1,3 +1,7 @@
+using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Infra.IoC;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<BankingDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BankingDBConnection"));
+});
+
+DependencyContainer.RegisterServices(builder.Services, builder.Configuration);
 
 var app = builder.Build();
 
