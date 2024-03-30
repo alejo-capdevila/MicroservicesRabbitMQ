fc001039e72d249346c0ccfc46bc5d58207244da
M1thr4ndir <sagaalejo@gmail.com>
2024-03-30 01:52:43 -0300

    feat: Implementar AccountService para la obtención de cuentas
    Este commit introduce la clase AccountService dentro de la aplicación de banca, implementando la interfaz IAccountService. El servicio facilita la obtención de cuentas utilizando una instancia inyectada de IAccountRepository.

---
+++ MicroRabbit.BakingApplication/Interfaces/IAccountService.cs
@@ -0,0 +1,9 @@
+﻿using MicroRabbit.Banking.Domain.Models;
+
+namespace MicroRabbit.BakingApplication.Interfaces
+{
+    public interface IAccountService
+    {
+        IEnumerable<Account> GetAccounts();
+    }
+}
