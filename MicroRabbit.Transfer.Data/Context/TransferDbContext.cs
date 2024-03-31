81016b52fbc0075c59d342206e4b209f9c25d112
M1thr4ndir <sagaalejo@gmail.com>
2024-03-31 18:22:38 -0300

    feat: Agregar archivos y estructura para manejo de transferencias
    Se han agregado los siguientes archivos y estructura para el manejo de transferencias en el proyecto:
    
    - Se ha creado la interfaz ITransferService en la carpeta Interfaces del proyecto MicroRabbit.Transfer.Application.
    - Se ha creado la clase TransferService en la carpeta Services del proyecto MicroRabbit.Transfer.Application.
    - Se ha creado la clase TransferDbContext en la carpeta Context del proyecto MicroRabbit.Transfer.Data, y se han agregado los archivos de migraciones generados por Entity Framework.
    - Se ha creado la clase TransferRepository en la carpeta Repository del proyecto MicroRabbit.Transfer.Data.
    - Se ha creado la interfaz ITransferRepository en la carpeta Interfaces del proyecto MicroRabbit.Transfer.Domain.
    - Se ha creado la clase TransferLog en la carpeta Models del proyecto MicroRabbit.Transfer.Domain.
    
    Además, se han ejecutado las migraciones en el contenedor Docker para crear las tablas correspondientes en la base de datos. Se ha implementado la lógica de servicios y repositorios, así como sus respectivas interfaces.

--- MicroRabbit.Transfer.Data/Context/TransferDbContext.cs
+++ MicroRabbit.Transfer.Data/Context/TransferDbContext.cs
@@ -1,11 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using MicroRabbit.Transfer.Domain.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace MicroRabbit.Transfer.Data.Context
 {
     public class TransferDbContext : DbContext
     {
-        protected TransferDbContext(DbContextOptions<TransferDbContext> options) : base(options)
+        public TransferDbContext(DbContextOptions<TransferDbContext> options) : base(options)
         {
         }
+
+        public DbSet<TransferLog> TransferLogs { get; set; }
     }
 }
